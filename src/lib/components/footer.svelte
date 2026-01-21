405c15289f334d12e90761bdef621ad1ebab182b
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-01-21 18:20:03 +0100

    feat(components): add footer with GitHub link

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Github from '$lib/assets/github.svelte';
+</script>
+
+<footer class="footer-center footer bg-base-200 p-4 text-sm text-gray-500 sm:footer-horizontal">
+	<p>Dieses Tool wird „as is“ bereitgestellt. Nutzung auf eigene Verantwortung.</p>
+	<a
+		href="https://github.com/BlackTiger007/video-fetch"
+		class="btn btn-square hover:btn-accent"
+		target="_blank"
+		rel="noopener noreferrer"
+		aria-label="GitHub Repository"
+	>
+		<Github></Github>
+	</a>
+</footer>
