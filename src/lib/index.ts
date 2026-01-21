3597ca918ffba25274de947e142bce3e3da2820c
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-01-21 18:20:05 +0100

    feat(lib): export maxConcurrency from public environment variable

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,3 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { env as public_env } from '$env/dynamic/public';
+
+export const maxConcurrency = parseInt(public_env.PUBLIC_MAX_CONCURRENCY || '5', 10); // maximal erlaubter Wert
