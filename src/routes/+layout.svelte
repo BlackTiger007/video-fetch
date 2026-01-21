f8d600bf598364f99a90f999c0835ec077772342
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-01-21 18:25:04 +0100

    feat(ui): add Navbar and Footer components to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Navbar from '$lib/components/navbar.svelte';
+	import Footer from '$lib/components/footer.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Video-Fetch</title>
 </svelte:head>
 
-{@render children()}
+<div class="flex min-h-screen flex-col bg-base-300">
+	<Navbar></Navbar>
+	<main class="flex w-full flex-1 flex-col items-center py-8">
+		{@render children?.()}
+	</main>
+	<Footer></Footer>
+</div>
