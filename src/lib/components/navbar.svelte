4212cc136005f019ecbd9bfb824a69dc143495d1
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-01-21 18:20:04 +0100

    feat(components): add basic navbar with theme selector

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,12 @@
+<div class="navbar bg-base-200 shadow-sm">
+	<div class="flex-1">
+		<h1 class="text-xl font-bold">Video-Fetch</h1>
+	</div>
+	<div class="flex-none">
+		<select class="select" title="Farbpalette">
+			<option selected>System</option>
+			<option>Dark</option>
+			<option>Light</option>
+		</select>
+	</div>
+</div>
