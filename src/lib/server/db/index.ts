4c3271aec8e93b69e2cfe359eb701f8d1313939f
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-01-30 00:13:00 +0100

    chore(db): enforce DATABASE_PATH at runtime initialization

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,6 +3,8 @@ import Database from 'better-sqlite3';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-const client = new Database(env.DATABASE_PATH ?? '/data/downloads.db');
+if (!env.DATABASE_PATH) throw new Error('DATABASE_PATH is not set');
+
+const client = new Database(env.DATABASE_PATH);
 
 export const db = drizzle(client, { schema });
