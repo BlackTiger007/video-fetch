8d86381b7e611ae2f4bad01ec50feafb55d69516
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-01-30 01:27:35 +0100

    ui: temporarily hide quality selection in add page

--- src/routes/add/+page.svelte
+++ src/routes/add/+page.svelte
@@ -157,13 +157,13 @@
 					/>
 				</label>
 
-				<label class="flex flex-col">
+				<!-- <label class="flex flex-col">
 					<span class="font-medium">Qualität</span>
 					<select name="quality" class="select-bordered select w-full">
 						<option value="highest">Beste Qualität</option>
 						<option value="lowest">Schlechteste Qualität</option>
 					</select>
-				</label>
+				</label> -->
 
 				<label class="flex flex-col">
 					<span class="font-medium">Optionaler Dateiname</span>
